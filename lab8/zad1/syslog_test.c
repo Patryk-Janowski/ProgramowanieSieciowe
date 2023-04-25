77f413b5ab9290d51b4df9830cf6b54cbeba0e05
Patryk Janowski <pkjanowski@gmail.com>
2023-04-25 19:33:46 +0200

    _

---
+++ lab8/zad1/syslog_test.c
@@ -0,0 +1,17 @@
+#include <syslog.h>
+#include <unistd.h>
+ 
+
+int main(int argc, char **argv){
+	setlogmask (LOG_UPTO (LOG_INFO));
+	
+	openlog (argv[0], LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL7);
+	
+	syslog (LOG_NOTICE, "Program started by User %d", getuid ());
+	syslog (LOG_NOTICE, "ERRNO = %m");
+	syslog (LOG_INFO, "A tree falls in a forest");
+	syslog (LOG_ERR, "A big tree falls in a forest");
+	syslog (LOG_LOCAL7, "local log 777");
+	
+	closelog ();
+}
